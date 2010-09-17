155b9b85af0068599ef2d54ffbeaf61e5ae2149b
Chris Boertien <chris.boertien@gmail.com>
2010-09-16 21:18:46 -0500

    fixup projects table
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,7 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @project }
+      format.json { render :json => @projects }
     end
   end
 
